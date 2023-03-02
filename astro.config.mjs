2491a1f4d0b89a5722ad9df23e1bc601d1740136
jomeswang <2019285005@email.szu.edu.cn>
2023-03-02 20:59:17 +0800

    ff

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,24 @@
-import { defineConfig } from 'astro/config'
-import vercel from '@astrojs/vercel/edge'
-import unocss from 'unocss/astro'
-import { presetUno } from 'unocss'
-import presetAttributify from '@unocss/preset-attributify'
-import solidJs from '@astrojs/solid-js'
+import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/edge';
+import unocss from 'unocss/astro';
+import { presetUno } from 'unocss';
+import presetAttributify from '@unocss/preset-attributify';
+import solidJs from '@astrojs/solid-js';
+
+// https://astro.build/config
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    unocss({
-      presets: [presetAttributify(), presetUno()]
-    }),
-    solidJs()
-  ],
+  integrations: [unocss({
+    presets: [presetAttributify(), presetUno()]
+  }), solidJs()],
   output: 'server',
-  adapter: vercel(),
   server: {
     port: +process.env.PORT || 3000,
     host: true
-  }
+  },
+  adapter: node({
+    mode: "standalone"
+  })
 });
